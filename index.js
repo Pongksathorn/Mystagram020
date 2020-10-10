f466084b56547bac2dbd4e4c5a03565fe9e144eb
Pongksathorn <614259020@webmail.npru.ac.th>
2020-10-10 20:45:58 +0700

    Pongksathorn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const path = require('path');
 app.engine('hbs', hbs({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/',(req,res)=>{
-//     res.send("<h1>My Portfolio</h1>");
-// });
+
 app.use('/', router);
 app.listen(
     PORT,
